c07d8a0d85a5b5e883eafed7510a027bd0700a8e
Crusader79 <schandler@tripleview.com>
2023-06-22 22:04:56 -0500

    modified hello world to hello first change

--- vs-2019-github-user-experience/Program.cs
+++ vs-2019-github-user-experience/Program.cs
@@ -6,7 +6,7 @@ namespace vs_2019_github_user_experience
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello first change!");
             Console.ReadLine();
         }
     }
